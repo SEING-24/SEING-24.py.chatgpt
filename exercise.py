dd2df58bfbfdfc5c3a5732a1a796b3fe9399a793
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2024-09-17 01:08:08 +0000

    Update OpenAI integration in exercise.py

--- exercise.py
+++ exercise.py
@@ -1,6 +1,35 @@
+
+import os
+from openai import OpenAI
+
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+    token = os.environ["GITHUB_TOKEN"]
+    endpoint = "https://models.inference.ai.azure.com"
+    model_name = "gpt-4o-mini"
+
+    client = OpenAI(
+        base_url=endpoint,
+        api_key=token,
+    )
+
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant.",
+            },
+            {
+                "role": "user",
+                "content": "What is the capital of France?",
+            }
+        ],
+        model=model_name,
+        temperature=1.0,
+        max_tokens=1000,
+        top_p=1.0
+    )
+
+    print(response.choices[0].message.content)
 
 if __name__=='__main__':
     main()
